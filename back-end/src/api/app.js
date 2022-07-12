cec6a9c89dade7813bed47fafe391e0eceaf3b55
Seu nome <senamatheus.job@gmail.com>
2022-07-12 18:05:58 -0300

    feat: adicionando permissão para api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { Routes } = require('../routes');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3002'
+}));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
