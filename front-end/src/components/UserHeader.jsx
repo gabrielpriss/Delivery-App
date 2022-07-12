3b761e7cc22c06c2e63f10757631a8d8ed00656c
Seu nome <senamatheus.job@gmail.com>
2022-07-12 19:28:02 -0300

    feat: Adição das funcionalidades do componente header

--- front-end/src/components/UserHeader.jsx
+++ front-end/src/components/UserHeader.jsx
@@ -4,19 +4,21 @@ import '../css/userHeader.css';
 
 export default function UserHeader() {
   const history = useHistory();
+  const user = JSON.parse(localStorage.getItem('user') || {});
+
   const exit = () => {
     localStorage.clear();
-    history.push('/register');
-  }
+    history.push('/login');
+  };
 
   return (
-    <div className='user-header'>
-      <div>
+    <div className="user-header">
+      <div className="user-first-container">
         <h2>Produtos</h2>
         <h2>Meus Pedidos</h2>
       </div>
-      <div>
-        <h2>Usuario</h2>
+      <div className="user-second-container">
+        <h2>{user.name}</h2>
         <button type="button" onClick={ exit }>
           Sair
         </button>
