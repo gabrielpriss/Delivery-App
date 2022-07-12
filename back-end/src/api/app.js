130dfbc0eb8a42809cac3803d2e34cc01dfb37e1
Seu nome <senamatheus.job@gmail.com>
2022-07-12 19:27:08 -0300

    fix: Correção da permissão para api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,9 +4,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3002'
-}));
+
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
