a543925591e1655d0c91e283fbf824867a0f46bc
Airton <junynnhoh@gmail.com>
2022-07-15 11:07:40 -0300

    adiciona rota de img backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const error = require('../middlewares/middleware.error');
 const { Routes } = require('../routes');
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/images', express.static(path.join(__dirname, '..', '..', 'assets', 'images')));
 
 app.use(Routes);
 
